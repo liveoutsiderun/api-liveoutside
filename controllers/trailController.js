3b08108b1d0070868f43bcf5aff6d3a49363f6eb
Daniel <danielsquinteros@gmail.com>
2022-04-09 15:50:25 -0400

    feat: add to endpoint trails basic

--- controllers/trailController.js
+++ controllers/trailController.js
@@ -1,9 +1,19 @@
-const AllRoutes = require('../static/AllRoutes.json');
+const AllRoutesDetail = require('../static/AllRoutesDetail.json');
+const AllRoutesBasic = require('../static/AllRoutesBasic.json');
 
 module.exports = {
+  getTrailsBasic: async (req, res) => {
+    try {
+      res.status(200).send(AllRoutesBasic);
+    } catch (error) {
+      res.status(404).send({
+        msg: 'Contact the administrator',
+      });
+    }
+  },
   getTrailById: async (req, res) => {
     const { id } = req.params;
-    const trail = AllRoutes.find(
+    const trail = AllRoutesDetail.find(
       (route) => route.id === Number(id),
     );
     try {
--- routes/trail.js
+++ routes/trail.js
@@ -4,6 +4,7 @@ const router = Router();
 const trailController = require('../controllers/trailController');
 const { validateJWT, isRole } = require('../middlewares');
 
+router.get('/trails/basic', trailController.getTrailsBasic);
 router.get('/trail/:id', [validateJWT, isRole], trailController.getTrailById);
 
 module.exports = router;
